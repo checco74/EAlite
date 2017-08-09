c835c0adf7c0613762abec5f94a3f1ff8f6aa60b
Florian Krull <flo.krull@gmail.com>
2017-08-09 20:05:50 +0200

    extract code from draw_fold_plots to get_plot_data allows calculation of plot data without drawing

--- EAtestslite/R/draw_fold_plots.R
+++ EAtestslite/R/draw_fold_plots.R
@@ -4,23 +4,67 @@ function (mydata.full = NULL, myannot.full = NULL, myparameters = init_parameter
 {
     plottype = "l"
 	defcolor = "#999999"
-    library(Hmisc, quietly = T, warn.conflicts = F)
     if (is.null(mydata.full) || is.null(myannot.full)) {
         return(NULL)
     }
+    
+    pd <- get_plot_data(pvals=mydata.full, 
+                        annot=myannot.full, 
+                        breaks=myparameters$mybreaks, 
+                        numof.breaks=myparameters$numof.breaks)
+    cat("plotting masters..\n")
+    plot(pd$datapoints, rep(1, max(10, length(pd$datapoints))),
+        type = plottype, lty = 2, lwd = 1.5, pch = 16, cex = 0.5, col = defcolor,
+        log = "y", xlab = "Nominal -log10(p)", ylab = "Fold enrichment",
+        xlim = c(0, 8), ylim = c(0.1, 10))
+    cat("plotting additional points..\n")
+    mycolors <- array(dim = length(pd$folds))
+    for (k in 1:length(pd$folds)) mycolors[k] <-
+        mycolorfunction(k, length(pd$folds), myparameters$figure.colors)
+    for (k in 1:length(pd$folds)) {
+        cat("plotting ", 'I', k, " -- color=", mycolors[k], "..\n", sep = "")
+        points(pd$datapoints, pd$folds[[k]]$y, type = plottype,
+            lwd = 2, col = mycolors[k], pch = 16)
+        # draw confidence intervals?
+        if (myparameters$do_draw_cis) {
+            points(pd$datapoints, pd$folds[[k]]$ci0, type = plottype, col = mycolors[k], lty = 2)
+            points(pd$datapoints, pd$folds[[k]]$ci1, type = plottype, col = mycolors[k], lty = 2)
+        }
+    }
+    cat("writing legends..\n")
+    annot_names <- unlist(lapply(pd$fold, function(x){x$name}))
+    legend("topleft", c("all SNPs", annot_names),
+        lwd = c(rep(2, length(annot_names) + 1)), bg = "white",
+        col = c(defcolor, mycolors[1:length(annot_names)]))
+}
+
+
+get_plot_data <- 
+function(pvals, annot, filter=NULL, breaks='auto', numof.breaks=4, verbose=T) 
+{
+    pd <- NULL
+    pd$datapoints <- NULL
+    pd$folds      <- list()
+    # example for list elements  
+    # pd$folds[[i]]$y      <- NULL 
+    # pd$folds[[i]]$ci0    <- NULL
+    # pd$folds[[i]]$ci1    <- NULL
+    # pd$folds[[i]]$name   <- NULL
+
+    library(Hmisc, quietly = T, warn.conflicts = F)
     neglog10_P_threshold <- 24
     tiny <- 1e-72
     cat("p-value genome-wide significance level is set to: ")
-    gws.level <- max(5e-08, 0.05/sum(!is.na(mydata.full)))
+    gws.level <- max(5e-08, 0.05/sum(!is.na(pvals)))
     cat(gws.level, "\n")
-    if (myparameters$verbose) cat("setting temporary data storage tables..\n")
-    myorder = order(mydata.full)
-    mydata <- mydata.full[myorder]
-    myannot <- myannot.full[myorder]
+    if (verbose) cat("setting temporary data storage tables..\n")
+    myorder = order(pvals)
+    mydata <- pvals[myorder]
+    myannot <- annot[myorder]
     myfilter = NULL
-    if ( is.null( myfilter.full ) ) {
+    if ( is.null(filter) ) {
         myfilter = array( TRUE, dim = c( length( mydata ), 1 ) )
-    } else { myfilter = myfilter.full[myorder,] }
+    } else { myfilter = filter[myorder,] }
     # filter NA values
     mymask <- !is.na(mydata) & !is.na(myannot)
     mydata <- mydata[mymask]
@@ -29,7 +73,7 @@ function (mydata.full = NULL, myannot.full = NULL, myparameters = init_parameter
     myorder <- myorder[mymask]
     # convert p-values to negative log scale [0,inf]
     neglog10_P_all <- -log10(mydata)
-    if (myparameters$verbose) {
+    if (verbose) {
         print("draw_qq_plots() neglog10_P_all:")
         print(summary(neglog10_P_all))
     }
@@ -48,22 +92,16 @@ function (mydata.full = NULL, myannot.full = NULL, myparameters = init_parameter
     # interesting p values
     cdftemp_all <- cumsum(histotemp_all$counts)
     datafilter <- histotemp_all$mids < -log10(gws.level) | histotemp_all$counts > 1
-    datapoints <- histotemp_all$mids[datafilter]
+    pd$datapoints <- histotemp_all$mids[datafilter]
+    # data struct to store plot data
     binconftemp_all <- binconf(cdftemp_all, n_all, method = "exact")
     binconftemp_all <- binconftemp_all[datafilter, ]
-    cat("plotting masters..\n")
-    plot(datapoints, rep(1, max(10, length(datapoints))),
-        type = plottype, lty = 2, lwd = 1.5, pch = 16, cex = 0.5, col = defcolor,
-        log = "y", xlab = "Nominal -log10(p)", ylab = "Fold enrichment",
-        xlim = c(0, 8), ylim = c(0.1, 10))
-    cat("plotting additional points..\n")
-    myannotq <- factorize_annotation(myannot, myparameters$mybreaks, myparameters$numof.breaks)
+    myannotq <- factorize_annotation(myannot, breaks, numof.breaks)
     myannotq_levels <- levels(as.factor(myannotq))
-    mycolors <- array(dim = length(myannotq_levels))
-    for (k in 1:length(myannotq_levels)) mycolors[k] <-
-        mycolorfunction(k, length(myannotq_levels), myparameters$figure.colors)
     for (k in 1:length(myannotq_levels)) {
-        cat("plotting ", 'I', k, " -- color=", mycolors[k], "..\n", sep = "")
+        fold <- NULL
+        fold$name <- as.character(myannotq_levels[k])
+        cat("calc ", 'I', k, "..\n", sep = "")
         neglog10_P <- -log10(mydata[myannotq == myannotq_levels[k]])
         neglog10_P <- neglog10_P[!is.na(neglog10_P)]
         n = length(neglog10_P)
@@ -77,24 +115,15 @@ function (mydata.full = NULL, myannot.full = NULL, myparameters = init_parameter
         binconftemp <- binconftemp[datafilter, ]
         # we plot (1 - binconftemp) ratios because we want to emphasize the fold enrichment on
         # the interesting end of the p value spectrum
-        if (length(-log10(1 - binconftemp[, 1])) > 0 && length(datapoints) > 0 && 
-            length(-log10(1 - binconftemp[, 1])) == length(datapoints)) {
-            foldtemp <- (1 - binconftemp[, 1])/(1 - binconftemp_all[,1] + tiny)
-            points(datapoints, foldtemp, type = plottype,
-                lwd = 2, col = mycolors[k], pch = 16)
-            # draw confidence intervals?
-            if (myparameters$do_draw_cis) {
-                foldtemp_2 <- (1 - binconftemp[, 2])/(1 - binconftemp_all[,2] + tiny)
-                foldtemp_3 <- (1 - binconftemp[, 3])/(1 - binconftemp_all[,3] + tiny)
-                points(datapoints, foldtemp_2, type = plottype, col = mycolors[k], lty = 2)
-                points(datapoints, foldtemp_3, type = plottype, col = mycolors[k], lty = 2)
-            }
+        if (length(-log10(1 - binconftemp[, 1])) > 0 && length(pd$datapoints) > 0 && 
+            length(-log10(1 - binconftemp[, 1])) == length(pd$datapoints)) {
+            fold$y <- (1 - binconftemp[, 1])/(1 - binconftemp_all[,1] + tiny)
+            # calc confidence intervals
+            fold$ci0 <- (1 - binconftemp[, 2])/(1 - binconftemp_all[,2] + tiny)
+            fold$ci1 <- (1 - binconftemp[, 3])/(1 - binconftemp_all[,3] + tiny)
         }
         else cat("warning: degenerate", 'I', k, "\n")
+        pd$folds[[k]] <- fold
     }
-    cat("writing legends..\n")
-    legend("topleft", c("all SNPs", myannotq_levels),
-        lwd = c(rep(2, length(myannotq_levels) + 1)), bg = "white",
-        col = c(defcolor, mycolors[1:length(myannotq_levels)]))
-    
+    pd
 }
